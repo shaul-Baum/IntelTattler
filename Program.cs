d603921e30785004842410d62eebfe474df7da3a
shaul baum <S0534156555@gmail.com>
2025-06-09 15:35:34 +0300

    4

--- Program.cs
+++ Program.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp9
+namespace IntelTettler
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine();
         }
     }
 }
